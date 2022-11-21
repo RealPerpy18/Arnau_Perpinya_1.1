9e9871e3ae815db1547d6e5651e19414101dcab5
RealPerpy18 <arpeto@inspalamos.cat>
2022-11-21 12:52:07 +0100

    No esta be encara

--- src/cipds9/uf2/practica/ArrayByteSearcher.java
+++ src/cipds9/uf2/practica/ArrayByteSearcher.java
@@ -1,11 +1,18 @@
 package cipds9.uf2.practica;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
 import static java.util.Arrays.copyOfRange;
 
-public class ArrayByteSearcher {
+public class ArrayByteSearcher extends RecursiveTask {
 
+private static final int THRESHOLD=25;
     byte[]Fitxer;
     byte[]Target;
 
@@ -14,29 +21,6 @@ public class ArrayByteSearcher {
         Target = target;
     }
 
-    public byte[] getFitxer() {
-
-        return Fitxer;
-    }
-
-    public byte[] getTarget() {
-
-        return Target;
-    }
-    public long[] calculosComplejos(){
-        byte[]auxFitxer = new byte[0];
-        long[]ret=new long[0];
-        for(int i=0;i<Fitxer.length;i++) {
-            if(Fitxer[i]==Target[0]) {
-                auxFitxer=copyOfRange(Fitxer,  i,  i+Target.length);
-                if(Arrays.equals(auxFitxer,Target)){
-                   ret= addArr(ret,i+1);
-                   i=i+(Target.length-1);
-                }
-            }
-        }
-        return ret;
-    }
     public long[] addArr(long[]a, long b){
         int n = a.length;
         long newArr[] = new long[n+1];
@@ -45,11 +29,63 @@ public class ArrayByteSearcher {
             newArr[i] = a[i];
         }
         newArr[n] = b;
-
-
-
-        return newArr;
+      return newArr;
     }
+
+    @Override
+    protected long[] compute() {
+
+        if (Fitxer.length<THRESHOLD){
+           return task();
+        }
+
+         else{
+
+            List<ArrayByteSearcher> subtasks =
+                    new ArrayList<ArrayByteSearcher>();
+            subtasks.addAll(createSubtasks());
+
+            for(ArrayByteSearcher subtask : subtasks){
+                subtask.fork();
+            }
+            for(ArrayByteSearcher subtask : subtasks) {
+               subtask.join();
+            }
+
+            return task();
+        }
+
+
+    }
+
+
+    private List<ArrayByteSearcher> createSubtasks() {
+        List<ArrayByteSearcher> subtasks =
+                new ArrayList<ArrayByteSearcher>();
+
+        ArrayByteSearcher subtask1 = new ArrayByteSearcher(copyOfRange(Fitxer,0,Fitxer.length/2),Target);
+        ArrayByteSearcher subtask2 = new ArrayByteSearcher(copyOfRange(Fitxer,Fitxer.length/2,Fitxer.length),Target);
+
+        subtasks.add(subtask1);
+        subtasks.add(subtask2);
+
+        return subtasks;
+    }
+    private long[] task(){
+        byte[]auxFitxer = new byte[0];
+        long[]ret=new long[0];
+        for(int i=0;i<Fitxer.length;i++) {
+            if(Fitxer[i]==Target[0]) {
+                auxFitxer=copyOfRange(Fitxer,  i,  i+Target.length);
+                if(Arrays.equals(auxFitxer,Target)){
+                    ret= addArr(ret,i+1);
+                    i=i+(Target.length-1);
+                }
+            }
+        }
+        return ret;
+    }
+
 }
 
 
--- src/cipds9/uf2/practica/FileScanner.java
+++ src/cipds9/uf2/practica/FileScanner.java
@@ -4,18 +4,24 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
 
 public class FileScanner {
 
     private static int FILE_MAX_SIZE=100000;
     private static String pathfile="C:\\apunts.txt";
-    private static String pathtarget="C:\\target.bin";
+    private static String pathtarget="C:\\target.txt";
 
     public static void main(String[] args) {
       //  int cores = Runtime.getRuntime().availableProcessors();
 
+        long t1=System.currentTimeMillis();
+        ForkJoinPool pool=new ForkJoinPool(2);
         ArrayByteSearcher abs=new ArrayByteSearcher(readFile(pathfile),readFile(pathtarget));
-        System.out.println(Arrays.toString(abs.calculosComplejos()));
+        long[]a= (long[]) pool.invoke(abs);
+        long t2=System.currentTimeMillis();
+        System.out.println(t2-t1);
+        System.out.println(Arrays.toString(a));
 
     }
     private static byte[] readFile(String path){
